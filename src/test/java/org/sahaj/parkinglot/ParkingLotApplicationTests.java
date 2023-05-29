477b641e77865345d26204fda78d8858c38e3ac4
vinayak03 <kadamvinayak03@gmail.com>
2023-05-29 11:50:36 +0530

    Adding sample application used

---
+++ src/test/java/org/sahaj/parkinglot/ParkingLotApplicationTests.java
@@ -0,0 +1,14 @@
+package org.sahaj.parkinglot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingLotApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+	}
+
+}
